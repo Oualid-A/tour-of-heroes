c2242abee262503ee4a3959f1d027d1e5e5b72fc
Oualid-A <oualid.achrif@ump.ac.ma>
2024-03-12 17:14:03 +0100

    add get data from server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,21 +8,29 @@ import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 @NgModule({
-    declarations: [
-        AppComponent,
-        MessagesComponent,
-        DashboardComponent,
-        // HeroesComponent
-    ],
-    providers: [],
-    bootstrap: [AppComponent],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        FormsModule,
-        HeroesComponent
-    ]
+  declarations: [
+    AppComponent,
+    MessagesComponent,
+    DashboardComponent,
+    HeroSearchComponent,
+    // HeroesComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HeroesComponent,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      dataEncapsulation: false,
+    }),
+  ],
 })
-export class AppModule { }
+export class AppModule {}
