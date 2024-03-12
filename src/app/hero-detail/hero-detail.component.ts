ff5442b4e0d58ce0c361f8632b24fe992d16f99c
Oualid-A <oualid.achrif@ump.ac.ma>
2024-03-12 16:30:19 +0100

    first commit

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { NgFor, NgIf, UpperCasePipe, Location } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css'],
+  standalone: true,
+  imports: [NgFor, FormsModule, NgIf, UpperCasePipe, HeroDetailComponent]
+})
+export class HeroDetailComponent  implements OnInit {
+  @Input() hero?: Hero;
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+  }
+  goBack(): void {
+    this.location.back();
+  }
+}
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+  standalone: true,
+  imports:[
+    NgFor,
+    FormsModule,
+    NgIf,
+    UpperCasePipe,
+    HeroDetailComponent
+  ]
+})
+export class HeroesComponent {
+  // heroes = HEROES;
+  heroes : Hero[]=[];
+  selectedHero?: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService,
+  ){}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero){
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void{
+    // this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().subscribe(
+      heroes => this.heroes = heroes
+    )
+  }
+}
